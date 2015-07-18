bc4bb1af1c797e68e03b34e062a5f9bf9c6750a5
JarviS <JarviS@Gokuls-iMac.local>
2015-07-19 00:49:44 +0530

    Added new stories sectionm TODO: add Spinner

--- app/js/application.js
+++ app/js/application.js
@@ -3,6 +3,8 @@
 
 var target = document.getElementById('main-container'); //Target element to render the components
 
+var items = [];
+
 var Header = React.createClass({
   render: function() {
     return(
@@ -13,12 +15,34 @@ var Header = React.createClass({
 });
 
 var MenuBar = React.createClass({
+  getInitialState: function() {
+    return {
+      isSelected: 'New'
+    }
+  },
+  handleClick: function(event) {
+    if (event.target.innerHTML === 'New') {
+      this.setState({
+        isSelected : 'New'
+      });
+    }
+    else if (event.target.innerHTML === 'Show') {
+      this.setState({
+        isSelected : 'Show'
+      }); 
+    }
+    else if (event.target.innerHTML === 'Jobs') {
+      this.setState({
+        isSelected : 'Jobs'
+      });
+    }
+  },
   render: function() {
     return(
-      <ul className="menu">
-        <li className="selected">New</li>
-        <li>Show</li>
-        <li>Jobs</li>
+      <ul className="menu" onClick={this.handleClick}>
+        <li className={this.state.isSelected === 'New' ? 'selected' : '' }>New</li>
+        <li className={this.state.isSelected === 'Show' ? 'selected' : '' }>Show</li>
+        <li className={this.state.isSelected === 'Jobs' ? 'selected' : '' }>Jobs</li>
       </ul>
     );
   }
@@ -40,49 +64,61 @@ var NewSection = React.createClass({
     }
   },
   componentDidMount: function() {
+    console.log(this);
+
+    this.getNewStories();
+    if (this.state.isSelected === 'New') {
+      this.getNewStories();
+    }
+    else if (this.state.isSelected === 'Show') {
+      this.getNewStories();
+    }
+    else if (this.state.isSelected === 'Jobs') {
+      this.getNewStories();
+    }
+  },
+  getNewStories: function () {
     var ids = [];
     var nextItem = [];
     var storiesUrl = 'https://hacker-news.firebaseio.com/v0/item/';
     var url = '';
     var score = '';
+    var paginationCount = 10;
 
     $.get(this.props.source, function (response) {
 
-      for (var i = 0; i <= 1; i++) {
+      for (var i = 0; i <= paginationCount; i++) {
 
         url = storiesUrl + response[i] + '.json';
         
         $.get(url, function (response) {
 
-          console.log(response)
-
           var domain = response.url ? response.url.split(':')[1].split('//')[1].split('/')[0] : ''; //To get domain name
 
           function timeAgo(ts) {
-           
-              var d = new Date(); // Gets the current time
-              var nowTs = Math.floor(d.getTime() / 1000);
-              var seconds = nowTs - ts;
+            var d = new Date(); // Gets the current time
+            var nowTs = Math.floor(d.getTime() / 1000);
+            var seconds = nowTs - ts;
 
-              // more that two days
-              if (seconds > 2*24*3600) {
-                 return 'a few days ago';
-              }
-              
-              // a day
-              if (seconds > 24*3600) {
-               return 'yesterday';
-              }
+            // more that two days
+            if (seconds > 2*24*3600) {
+               return 'a few days ago';
+            }
+            
+            // a day
+            if (seconds > 24*3600) {
+             return 'yesterday';
+            }
 
-              if (seconds > 3600) {
-               return 'a few hours ago';
-              }
-              if (seconds > 1800) {
-               return 'Half an hour ago';
-              }
-              if (seconds > 60) {
-                return Math.floor(seconds/60) + ' minutes ago';
-              }
+            if (seconds > 3600) {
+             return 'a few hours ago';
+            }
+            if (seconds > 1800) {
+             return 'Half an hour ago';
+            }
+            if (seconds > 60) {
+              return Math.floor(seconds/60) + ' minutes ago';
+            }
           }
 
          
@@ -94,6 +130,13 @@ var NewSection = React.createClass({
             score = response.score + ' Point '
           }
 
+          response.isLoaded = true;
+          response.domain = domain;
+
+          items.push({
+            response: response
+          })
+
           if (this.isMounted()) {
             this.setState({
               id: response.id,
@@ -103,8 +146,7 @@ var NewSection = React.createClass({
               author: response.by,
               type: response.type,
               domain: domain,
-              points: score,
-              isLoaded: true
+              points: score
             });
           }  
         }.bind(this));
@@ -114,21 +156,26 @@ var NewSection = React.createClass({
   },
   render: function() {
     return(
-      <div className="content">
-        
-        <img title="loader" src="../images/spinner.gif" className={this.state.isLoaded ? 'hide': ''}/>
-    
-        <div className={this.state.isLoaded ? '': 'hide'}>
-          
-          <a href={this.state.url} target="_blank">{this.state.title}</a> <span title="Domain">({this.state.domain})</span>  
-          
-          <div className="bottom-content">
-            <span>{this.state.points}</span>
-            <span className="author">by {this.state.author} </span>
-            <span>| {this.state.time}</span>
-          </div>
+      <div>
+      {
+        items.map(function (data) {
+        return <div className="content" key={data.response.id}> 
+          <img title="loader" src="../images/spinner.gif" className={data.response.isLoaded ? 'hide': ''}/>
+          <div className={data.response.isLoaded ? '': 'hide'}>
+            
+            <a href={data.response.url} target="_blank">{data.response.title}</a> <span className={data.response.domain ? '': 'hide'}>(</span><span title="Domain">{data.response.domain}</span><span>)</span>
+            
+            <div className="bottom-content">
+              <span>{data.response.points}</span>
+              <span className="author">by {data.response.author} </span>
+              <span>| {data.response.time}</span>
+            </div>
 
+            <span className="type">#{data.response.type}</span>
+          </div>
         </div>
+        })
+      }
       </div>
     );
   }
@@ -148,8 +195,4 @@ var ContainerBox = React.createClass({
   }
 });
 
-
-
-
 React.render(<ContainerBox />, target);
-
