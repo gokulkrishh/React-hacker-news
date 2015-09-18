fc546d2f4060e06a7dfb56962135ba2937d9b023
gokulkrishh <krishnangokul9@gmail.com>
2015-09-18 16:30:04 +0530

    Added babel es6 library

--- app/js/spinner.jsx
+++ app/js/spinner.jsx
@@ -4,9 +4,9 @@
 
 'use strict';
 
-var Spinner = React.createClass({
+const Spinner = React.createClass({
 	
-  render: function() {
+  render() {
     return(
       <svg className="spinner" width="50px" height="50px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
    			<circle className="path" fill="none" strokeWidth="5" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
