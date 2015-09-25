6982a6b7fe5c5624713ca8df7e183f2ad085480b
gokulkrishh <krishnangokul9@gmail.com>
2015-09-25 18:26:50 +0530

    Logo color change

--- app/js/components/feeds/aboutContent.jsx
+++ app/js/components/feeds/aboutContent.jsx
@@ -7,7 +7,7 @@ const AboutContent = React.createClass({
     	<div className="content-container">
       	<div className="content about-page">
       		<h1 className="heading">About</h1>
-      		<p className="about-content">The HackerNews site is create using <a href="https://github.com/HackerNews/API">API</a> provided by <a href="https://github.com/HackerNews">HackerNews</a>.
+      		<p className="about-content">The HackerNews site is created using <a href="https://github.com/HackerNews/API">API</a> provided by <a href="https://github.com/HackerNews">HackerNews</a>.
       		</p>
 
       		<h1 className="heading">Contributor</h1>
@@ -19,7 +19,6 @@ const AboutContent = React.createClass({
 			    <h1 className="heading">
 			    	<iframe src="https://ghbtns.com/github-btn.html?user=gokulkrishh&repo=React-hacker-news&type=star&count=true&size=large" frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
 			    	<iframe src="https://ghbtns.com/github-btn.html?user=gokulkrishh&repo=React-hacker-news&type=fork&count=true&size=large" frameborder="0" scrolling="0" width="158px" height="30px"></iframe>
-
 			    </h1>
 	     	</div>
       </div>
